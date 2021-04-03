e94fac3e66048bbd2d43cd98a23a3d9a17911e7e
Kolja Dummann <kolja@hey.com>
2021-04-03 11:30:35 +0200

    remove unused imports

--- web/test/ApplicationTest.kt
+++ web/test/ApplicationTest.kt
@@ -1,20 +1,7 @@
 package ws.logv.hosting
 
 import cloud.skadi.web.hosting.mainModule
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
 import io.ktor.http.*
-import io.ktor.html.*
-import kotlinx.html.*
-import kotlinx.css.*
-import io.ktor.content.*
-import io.ktor.http.content.*
-import io.ktor.features.*
-import io.ktor.auth.*
-import com.fasterxml.jackson.databind.*
-import io.ktor.jackson.*
 import io.ktor.locations.*
 import io.ktor.server.testing.*
 import org.junit.jupiter.api.Test
